58957843aa55b67a9ef6c6bb2f963af1c66803b3
Ильшат Исмагилов <starkylife@Ilsats-MacBook-Air.local>
2020-03-12 21:51:39 +0300

    refactor(*): remove unnecessary files and folders

--- module_1/task1.js
+++
@@ -1,9 +0,0 @@
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('readable', () => {
-  let chunk;
-  while ((chunk = process.stdin.read()) !== null) {
-    const reversed = chunk.replace('\n', '').split('').reverse().join('');
-    process.stdout.write(`${reversed}\n`);
-  }
-});
